11821cd70e3321ec87e03e9f805eb739d34a3071
yassa76 <yassassin@gmail.com>
2026-07-22 16:22:51 +0200

    Update page.tsx

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function TaskDetailPage({ params }: { params: { id: string 
   return (
     <div>
       <NavBar />
-      <main className="max-w-6xl mx-auto px-4 sm:px-6 py-6">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 py-6">
         <TaskDetailView taskId={params.id} />
       </main>
     </div>
