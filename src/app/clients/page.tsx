3bc0c1d0e5013cc391ec2f1b75181f82793a3e97
yassa76 <yassassin@gmail.com>
2026-07-22 11:59:41 +0200

    Create page.tsx

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '@/lib/auth'
+import NavBar from '@/components/NavBar'
+import ClientsView from '@/components/ClientsView'
+
+export default async function ClientsPage() {
+  const session = await getServerSession(authOptions)
+  if (!session) redirect('/login')
+
+  return (
+    <div>
+      <NavBar />
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 py-6">
+        <ClientsView />
+      </main>
+    </div>
+  )
+}
