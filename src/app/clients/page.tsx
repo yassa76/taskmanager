bfc4f9a3f6968a3274d16455b37ea10b861bbb84
yassa76 <yassassin@gmail.com>
2026-07-22 16:21:12 +0200

    Update page.tsx

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -11,7 +11,7 @@ export default async function ClientsPage() {
   return (
     <div>
       <NavBar />
-      <main className="max-w-4xl mx-auto px-4 sm:px-6 py-6">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 py-6">
         <ClientsView />
       </main>
     </div>
