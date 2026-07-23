2caa19eecbdf217de616fbd60ca33dd684145da6
yassa76 <yassassin@gmail.com>
2026-07-23 11:58:01 +0200

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import ThemeApplier from './ThemeApplier'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <ThemeApplier />
+      {children}
+    </SessionProvider>
+  )
 }
