3e53b44bf21ec9778b05dbb0a1c66a32fdb1d770
yassa76 <yassassin@gmail.com>
2026-07-23 09:50:41 +0200

    Create page.tsx

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '@/lib/auth'
+import NavBar from '@/components/NavBar'
+import HomeView from '@/components/HomeView'
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions)
+  if (!session) redirect('/login')
+
+  return (
+    <div>
+      <NavBar />
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 py-6">
+        <HomeView userName={session.user?.name || session.user?.email || ''} />
+      </main>
+    </div>
+  )
+}
