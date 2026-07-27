98741a1c64b16d25caf7c7692a4e634e992ff799
yassa76 <yassassin@gmail.com>
2026-07-27 10:44:32 +0200

    Update TasksView.tsx

--- src/components/TasksView.tsx
+++ src/components/TasksView.tsx
@@ -69,7 +69,8 @@ export default function TasksView() {
     () =>
       team
         .filter((t) => t.status !== 'inactive' && t.matchedUser)
-        .map((t) => ({ id: t.matchedUser!.id, name: t.matchedUser!.name || t.email, email: t.email })),
+        .map((t) => ({ id: t.matchedUser!.id, name: t.matchedUser!.name || t.email, email: t.email }))
+        .sort((a, b) => a.name.localeCompare(b.name)),
     [team]
   )
 
